43b8b964e346211f8a4ba8631bb543647635645f
Dilliya <dilusha.37.writer@gmail.com>
2026-05-05 17:44:59 +0400

    do 1

--- index.js
+++ index.js
@@ -35,6 +35,14 @@ class Component {
 }
 
 class TodoList extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      tasks : ["Сделать домашку", "Сделать практику", "Пойти домой"],
+    }
+  }
+
   render() {
     return createElement("div", { class: "todo-list" }, [
       createElement("h1", {}, "TODO List"),
@@ -46,23 +54,16 @@ class TodoList extends Component {
         }),
         createElement("button", { id: "add-btn" }, "+"),
       ]),
-      createElement("ul", { id: "todos" }, [
-        createElement("li", {}, [
-          createElement("input", { type: "checkbox" }),
-          createElement("label", {}, "Сделать домашку"),
-          createElement("button", {}, "🗑️")
-        ]),
-        createElement("li", {}, [
-          createElement("input", { type: "checkbox" }),
-          createElement("label", {}, "Сделать практику"),
-          createElement("button", {}, "🗑️")
-        ]),
-        createElement("li", {}, [
-          createElement("input", { type: "checkbox" }),
-          createElement("label", {}, "Пойти домой"),
-          createElement("button", {}, "🗑️")
-        ]),
-      ]),
+      
+      createElement("ul", { id: "todos" }, 
+        this.state.tasks.map(task => 
+          createElement("li", {}, [
+            createElement("input", { type: "checkbox" }),
+            createElement("label", {}, task),
+            createElement("button", {}, "🗑️")
+          ])
+        )
+      )
     ]);
   }
 }
